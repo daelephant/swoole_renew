6971dbe52133c4fd137e264543e24bbf3f0a0131
daelephant <2224168716@qq.com>
2019-06-13 17:33:21 +0800

    coroutine redis1

--- demo/io/redis.php
+++ demo/io/redis.php
@@ -8,24 +8,27 @@
 
 //$redisClient = new swoole_redis;// Swoole\Redis
 $redisClient = new Swoole\Coroutine\Redis();// Swoole\Redis
-$redisClient->connect('127.0.0.1', 6379, function(swoole_redis $redisClient, $result) {
-    echo "connect".PHP_EOL;
-    var_dump($result);
 
-    // 同步 redis (new Redis())->set('key',2);
-    $redisClient->set('singwa_1', time(), function(swoole_redis $redisClient, $result) {
-        var_dump($result);
-    });
-
-    $redisClient->get('singwa_1', function(swoole_redis $redisClient, $result) {
-        var_dump($result);
-        $redisClient->close();
-    });
-    $redisClient->keys('*gw*', function(swoole_redis $redisClient, $result) {
-        var_dump($result);
-        $redisClient->close();
-    });
-
-});
+$redisClient->connect('127.0.0.1', 6379);
+$val = $redis->get('key');
+//$redisClient->connect('127.0.0.1', 6379, function(swoole_redis $redisClient, $result) {
+//    echo "connect".PHP_EOL;
+//    var_dump($result);
+//
+//    // 同步 redis (new Redis())->set('key',2);
+//    $redisClient->set('singwa_1', time(), function(swoole_redis $redisClient, $result) {
+//        var_dump($result);
+//    });
+//
+//    $redisClient->get('singwa_1', function(swoole_redis $redisClient, $result) {
+//        var_dump($result);
+//        $redisClient->close();
+//    });
+//    $redisClient->keys('*gw*', function(swoole_redis $redisClient, $result) {
+//        var_dump($result);
+//        $redisClient->close();
+//    });
+//
+//});
 
 echo "start".PHP_EOL;
