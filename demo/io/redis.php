4c73a5720e693f728a2be236c82d743f968b3fe7
daelephant <2224168716@qq.com>
2019-06-13 17:29:36 +0800

    coroutine redis

--- demo/io/redis.php
+++ demo/io/redis.php
@@ -6,7 +6,8 @@
  * Time: 上午1:12
  */
 
-$redisClient = new swoole_redis;// Swoole\Redis
+//$redisClient = new swoole_redis;// Swoole\Redis
+$redisClient = new Swoole\Coroutine\Redis();// Swoole\Redis
 $redisClient->connect('127.0.0.1', 6379, function(swoole_redis $redisClient, $result) {
     echo "connect".PHP_EOL;
     var_dump($result);
