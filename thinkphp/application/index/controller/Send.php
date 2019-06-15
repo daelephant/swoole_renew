636f59ef9cdecd309004979745e5e1eec86fecf7
daelephant <2224168716@qq.com>
2019-06-15 18:55:34 +0800

    SMS async Redis3

--- thinkphp/application/index/controller/Send.php
+++ thinkphp/application/index/controller/Send.php
@@ -36,7 +36,12 @@ class Send
 //        return Util::show(config('code.success'), 'ok');
         try {
             $response = $Sms->sendSms($phoneNum, $code ,$expire);
-            echo $response;
+            $content = date("Ymd H:i:s").$response.PHP_EOL;
+            swoole_async_writefile(__DIR__."/runTime.log", $content, function($filename){
+                // todo
+                echo "success".PHP_EOL;
+            }, FILE_APPEND);
+
         }catch (\Exception $e) {
             // todo
             return Util::show(config('code.error'), '短信第三方内部异常');
