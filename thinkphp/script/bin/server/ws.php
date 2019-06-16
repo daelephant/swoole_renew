39d0e943face641e84dbd3c5eb4267cd480f7fdb
daelephant <2224168716@qq.com>
2019-06-16 17:33:39 +0800

    wx server

--- thinkphp/script/bin/server/ws.php
+++ thinkphp/script/bin/server/ws.php
@@ -20,7 +20,7 @@ class Ws {
             [
                 'enable_static_handler' => true,
 //                'document_root' => "/home/work/swoole_renew/thinkphp/public/static",
-                'document_root' => "/Users/yin/project/swoole-renew/thinkphp/public/static",
+                'document_root' => "/home/work/swoole_renew/thinkphp/public/static",
                 'worker_num' => 4,
                 'task_worker_num' => 4,
             ]
