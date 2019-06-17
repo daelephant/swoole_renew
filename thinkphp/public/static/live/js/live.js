3edec588012349d1b91f29b8bb19eac4ebf9edc6
daelephant <2224168716@qq.com>
2019-06-17 08:07:05 +0800

    push ceshi 提升js

--- thinkphp/public/static/live/js/live.js
+++ thinkphp/public/static/live/js/live.js
@@ -6,6 +6,29 @@ var websocket = new WebSocket(wsUrl);
 websocket.onopen = function(evt) {
     // websocket.send("live-wesocketJs-success");
 }
+function push(data) {
+    data = JSON.parse(data);
+    html = '<div class="frame">';
+    html += '<h3 class="frame-header">';
+    html += '<i class="icon iconfont icon-shijian"></i>第'+data.type+'节 09：30' ;
+    html += '</h3>' ;
+    html += '<div class="frame-item">';
+    html += '<span class="frame-dot"></span>';
+    html += '<div class="frame-item-author">' ;
+    if(data.logo){
+        html += '<img src="'+data.logo+'" width="20px" height="20px" /> ';
+    }
+    html += data.title;
+    html += '</div>';
+    html += '<p>'+data.content+'</p>';
+    if(data.image){
+        html += '<p><img src="'+data.image+'" width="40%"></p>';
+    }
+    html += '</div>';
+    html += '</div>';
+    $('#match-result').prepend(html);
+    console.log("into:" + data.content);
+}
 
 // 实例化 onmessage
 websocket.onmessage = function(evt) {
